c5777bbacc6be29221ef39835c1baaac1754afbf
pycuber <pete4radio@icloud.com>
2025-06-08 15:32:47 -0700

    improved copy, added SAMWISE

---
+++ SAMWISE.h
@@ -0,0 +1,3 @@
+#include "boards/pico2.h"
+#undef PICO_RP2350A
+#define PICO_RP2350B 1
--- do_copy.sh
+++ do_copy.sh
@@ -1,3 +1,7 @@
+##  First ones are for bash, last three are for windows.
 cp * ~/.pico-sdk/sdk/2.1.1/src/boards/include/boards/
 cp * ~/.pico-sdk/sdk/2.0.0/src/boards/include/boards/
 cp * ~/.pico-sdk/sdk/2.1.0/src/boards/include/boards/
+copy * ~/.pico-sdk/sdk/2.1.1/src/boards/include/boards/
+copy * ~/.pico-sdk/sdk/2.0.0/src/boards/include/boards/
+copy * ~/.pico-sdk/sdk/2.1.0/src/boards/include/boards/
