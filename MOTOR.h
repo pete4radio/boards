995ae338c1a73b1d3c80f7dd3e347bf75a3a1182
pycuber <134294337+pete4radio@users.noreply.github.com>
2025-06-07 19:53:13 -0700

    Added Motor

---
+++ MOTOR.h
@@ -0,0 +1,151 @@
+#include "boards/pico2.h"
+#undef PICO_RP2350A
+#define PICO_RP2350B 1
+
+#ifndef FGOUT_1
+#define FGOUT_1 (0)
+#endif
+
+#ifndef nSCS_1
+#define nSCS_1 (1)
+#endif
+
+#ifndef BRAKE_1
+#define BRAKE_1 (2)
+#endif
+
+#ifndef PWM_1
+#define PWM_1 (3)
+#endif
+
+#ifndef nSLEEP_1
+#define nSLEEP_1 (4)
+#endif
+
+#ifndef nSCS_2
+#define nSCS_2 (5)
+#endif
+
+#ifndef DRVOFF_1
+#define DRVOFF_1 (6)
+#endif
+
+#ifndef FGOUT_2
+#define FGOUT_2 (7)
+#endif
+
+#ifndef PWM_2
+#define PWM_2 (8)
+#endif
+
+#ifndef BRAKE_2
+#define BRAKE_2 (9)
+#endif
+
+#ifndef nFAULT_1
+#define nFAULT_1 (10)
+#endif
+
+#ifndef nSLEEP_2
+#define nSLEEP_2 (11)
+#endif
+
+#ifndef nFAULT_2
+#define nFAULT_2 (12)
+#endif
+
+#ifndef DRVOFF_2
+#define DRVOFF_2 (13)
+#endif
+
+#ifndef FGOUT_3
+#define FGOUT_3 (14)
+#endif
+
+#ifndef PWM_3
+#define PWM_3 (15)
+#endif
+
+#ifndef BRAKE_3
+#define BRAKE_3 (16)
+#endif
+
+#ifndef nFAULT_3
+#define nFAULT_3 (17)
+#endif
+
+#ifndef nSLEEP_3
+#define nSLEEP_3 (18)
+#endif
+
+#ifndef DRVOFF_3
+#define DRVOFF_3 (20)
+#endif
+
+#ifndef nSCS_4
+#define nSCS_4 (21)
+#endif
+
+#ifndef PWM_4
+#define PWM_4 (22)
+#endif
+
+#ifndef BRAKE_4
+#define BRAKE_4 (23)
+#endif
+
+#ifndef FGOUT_4
+#define FGOUT_4 (24)
+#endif
+
+#ifndef nSLEEP_4
+#define nSLEEP_4 (25)
+#endif
+
+#ifndef nFAULT_4
+#define nFAULT_4 (26)
+#endif
+
+#ifndef DRVOFF_4
+#define DRVOFF_4 (27)
+#endif
+
+#ifndef SDO0
+#define SDO0 (32)
+#endif
+
+#ifndef MOTOR_pGOOD
+#define MOTOR_pGOOD (33)
+#endif
+
+#ifndef MOTOR_EN
+#define MOTOR_EN (34)
+#endif
+
+#ifndef SDI0
+#define SDI0 (35)
+#endif
+
+#ifndef SDA_PWR
+#define SDA_PWR (36)
+#endif
+
+#ifndef SCL_PWR
+#define SCL_PWR (37)
+#endif
+
+#ifndef SCLK0
+#define SCLK0 (38)
+#endif
+
+#ifndef MOTOR_TX
+#define MOTOR_TX (40)
+#endif
+
+#ifndef MOTOR_RX
+#define MOTOR_RX (41)
+#endif
+
+#ifndef WATCHDOG_FEED
+#define WATCHDOG_FEED (42)
+#endif
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # boards
 Board pinouts for SAMWISE and its test pucks
+
+These go into ~/.pico-sdk/sdk/2.1.1/src/boards/include/boards/
